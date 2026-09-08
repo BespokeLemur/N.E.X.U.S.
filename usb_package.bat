de20ca2b3d07d8b9bf07b6af82923115dd1e4064
BespokeLemur <bespoke@nexus.dev>
2026-09-08 16:25:06 +0300

    fix: Trim spaces from USB drive letter input in usb_package.bat

--- usb_package.bat
+++ usb_package.bat
@@ -12,9 +12,11 @@ set /p USB_LETTER=Surucu Harfi:
 
 if "%USB_LETTER%"=="" goto ERROR
 
-set TARGET=%USB_LETTER%:\
+:: Strip spaces
+set USB_LETTER=%USB_LETTER: =%
+set TARGET=%USB_LETTER:~0,1%:
 
-if not exist "%TARGET%" (
+if not exist "%TARGET%\" (
     echo.
     echo =======================================================
     echo [HATA] "%TARGET%" surucusu bulunamadi!
@@ -36,15 +38,22 @@ mkdir "%TARGET%Sifrelerim_Vault" 2>nul
 echo [2/3] N.E.X.U.S. derlenmis dosyalari USB'ye kopyalaniyor...
 xcopy /E /Y /I "dist\*" "%TARGET%N.E.X.U.S\"
 
-echo [3/3] USB Baslatma scripti olusturuluyor...
+echo [3/3] USB Baslatma scripti ve Portable Sunucu olusturuluyor...
 (
   echo @echo off
-  echo title N.E.X.U.S. USB Launcher
+  echo title N.E.X.U.S. Portable USB Environment
   echo color 0A
-  echo echo ===================================================
-  echo echo [N.E.X.U.S.] USB Arayuzu Baslatiliyor...
-  echo echo ===================================================
-  echo powershell -NoProfile -Command "Start-Process '%%~dp0index.html'"
+  echo cls
+  echo =======================================================
+  echo     N.E.X.U.S. USB PORTABLE WEB SUNUCUSU BASLATILIYOR
+  echo =======================================================
+  echo.
+  echo [1/2] Yerel port ^(http://localhost:5500^) hazirlaniyor...
+  echo [2/2] Tarayici otomatik aciliyor...
+  echo.
+  echo Sunucuyu durdurmak icin bu pencereyi kapatabilirsiniz.
+  echo.
+  echo powershell -NoProfile -ExecutionPolicy Bypass -Command "$dir='%%~dp0'; $port=5500; $listener = New-Object System.Net.HttpListener; $listener.Prefixes.Add('http://localhost:'+$port+'/'); $listener.Start(); Start-Process ('http://localhost:'+$port); while ($listener.IsListening) { try { $ctx = $listener.GetContext(); $req = $ctx.Request; $res = $ctx.Response; $urlPath = $req.Url.AbsolutePath.TrimStart('/'); if ([string]::IsNullOrEmpty($urlPath)) { $urlPath = 'index.html' }; $filePath = [System.IO.Path]::Combine($dir, $urlPath); if (Test-Path $filePath -PathType Leaf) { $bytes = [System.IO.File]::ReadAllBytes($filePath); $ext = [System.IO.Path]::GetExtension($filePath); switch ($ext) { '.html' { $res.ContentType = 'text/html; charset=utf-8' } '.js' { $res.ContentType = 'application/javascript; charset=utf-8' } '.css' { $res.ContentType = 'text/css' } '.json' { $res.ContentType = 'application/json' } '.png' { $res.ContentType = 'image/png' } '.ico' { $res.ContentType = 'image/x-icon' } default { $res.ContentType = 'application/octet-stream' } }; $res.ContentLength64 = $bytes.Length; $res.OutputStream.Write($bytes, 0, $bytes.Length) } else { $res.StatusCode = 404 }; $res.OutputStream.Close() } catch {} }"
 ) > "%TARGET%N.E.X.U.S\N.E.X.U.S_BASLAT.bat"
 
 echo.
@@ -53,7 +62,7 @@ echo [TEBRIKLER] N.E.X.U.S. USB Belleginize Basariyla Yuklendi!
 echo =======================================================
 echo.
 echo USB Belleginizdeki "%TARGET%N.E.X.U.S\N.E.X.U.S_BASLAT.bat"
-echo veya "index.html" dosyasina cift tiklayarak kullanabilirsiniz.
+echo dosyasina cift tiklayarak N.E.X.U.S.'u aninda acabilirsiniz.
 echo.
 pause
 exit
