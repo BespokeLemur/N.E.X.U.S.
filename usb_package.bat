60606865ad1a8abb419818fe9deed63b3bd25d37
BespokeLemur <bespoke@nexus.dev>
2026-09-08 16:15:02 +0300

    fix: Add drive existence check in usb_package.bat installer

--- usb_package.bat
+++ usb_package.bat
@@ -1,5 +1,5 @@
 @echo off
-:: N.E.X.U.S. USB Flash Drive Installer & Packaging Tool
+:: N.E.X.U.S. USB Flash Drive Installer & Packaging Tool v3.2
 title N.E.X.U.S. USB Installer
 color 0A
 cls
@@ -11,7 +11,20 @@ echo Lutfen USB belleginizin surucu harfini girin (Ornegin E veya F):
 set /p USB_LETTER=Surucu Harfi: 
 
 if "%USB_LETTER%"=="" goto ERROR
+
 set TARGET=%USB_LETTER%:\
+
+if not exist "%TARGET%" (
+    echo.
+    echo =======================================================
+    echo [HATA] "%TARGET%" surucusu bulunamadi!
+    echo Lutfen USB belleginizin bilgisayara takili oldugundan ve
+    echo "Bu PC" altinda harfinin göründugunden emin olun.
+    echo =======================================================
+    echo.
+    pause
+    exit
+)
 
 echo.
 echo [1/3] Klasor yapisi olusturuluyor (%TARGET%)...
@@ -20,7 +33,7 @@ mkdir "%TARGET%Projelerim" 2>nul
 mkdir "%TARGET%Fotograflarim" 2>nul
 mkdir "%TARGET%Sifrelerim_Vault" 2>nul
 
-echo [2/3] N.E.X.U.S. derlenmis dosyaları USB'ye kopyalaniyor...
+echo [2/3] N.E.X.U.S. derlenmis dosyalari USB'ye kopyalaniyor...
 xcopy /E /Y /I "dist\*" "%TARGET%N.E.X.U.S\"
 
 echo.
